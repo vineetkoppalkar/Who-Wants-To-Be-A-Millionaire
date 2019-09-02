19eb6fce6b3c2b3d61f3a73019cae61200b3807b
Vineet <koppalkarvineet@gmail.com>
2019-09-01 23:09:14 -0400

    Shuffled question options

--- src/components/QuestionBoard.js
+++ src/components/QuestionBoard.js
@@ -4,25 +4,35 @@ import "./QuestionBoard.scss";
 import Paper from "@material-ui/core/Paper";
 
 class QuestionBoard extends Component {
+  shuffle = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  };
+
   render() {
     const { question, onOptionSelect } = this.props;
+    let shuffledOptions = [];
+    if (question) {
+      shuffledOptions = this.shuffle(
+        [question.correct_answer].concat(question.incorrect_answers)
+      );
+    }
+
     return (
       <div>
         {question ? (
           <Paper className="question-board">
-            <h3>{question.difficulty}</h3>
             <h3>{unescape(question.question)}</h3>
             <ul>
-              <li onClick={() => onOptionSelect(question.correct_answer)}>
-                {question.correct_answer}
-              </li>
-              {question.incorrect_answers.map(incorrect_answer => {
+              {shuffledOptions.map(option => {
                 return (
-                  <li
-                    key={incorrect_answer}
-                    onClick={() => onOptionSelect(incorrect_answer)}
-                  >
-                    {incorrect_answer}
+                  <li key={option} onClick={() => onOptionSelect(option)}>
+                    {option === question.correct_answer
+                      ? option + " [*]"
+                      : option}
                   </li>
                 );
               })}
