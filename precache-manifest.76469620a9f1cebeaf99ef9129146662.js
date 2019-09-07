6fac6d36aefbf22bdbfdfde6996ce4d7185b35ff
Vineet <koppalkarvineet@gmail.com>
2019-09-07 16:31:39 -0400

    Updates

---
+++ precache-manifest.76469620a9f1cebeaf99ef9129146662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa7f7d0844e972e091105eb67d5b86b",
+    "url": "/Who-Wants-To-Be-A-Millionaire/index.html"
+  },
+  {
+    "revision": "6a24368165c6c1158f5b",
+    "url": "/Who-Wants-To-Be-A-Millionaire/static/css/main.98278829.chunk.css"
+  },
+  {
+    "revision": "c8bf0427f80c1cb748db",
+    "url": "/Who-Wants-To-Be-A-Millionaire/static/js/2.6ce759e6.chunk.js"
+  },
+  {
+    "revision": "6a24368165c6c1158f5b",
+    "url": "/Who-Wants-To-Be-A-Millionaire/static/js/main.5987b627.chunk.js"
+  },
+  {
+    "revision": "58541b0634f44ee54cdb",
+    "url": "/Who-Wants-To-Be-A-Millionaire/static/js/runtime~main.d1df3868.js"
+  },
+  {
+    "revision": "a8954dc52b56d3b1515f93416d7a4335",
+    "url": "/Who-Wants-To-Be-A-Millionaire/static/media/logo.a8954dc5.png"
+  }
+]);
